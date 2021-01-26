93f36c2319771185d586fb468ef4a4fafaa92bae
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2021-01-26 12:08:46 -0700

    added dynamic camera controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 13f;
+    public float pitch = 7f;
+    public float yawSpeed = 100f;
+
+    private float currentZoom = 8f;
+    private float CurrentYaw = 0f;
+ 
+   
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        CurrentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, CurrentYaw);
+    }
+}
